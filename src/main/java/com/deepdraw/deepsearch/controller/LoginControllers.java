c12ce7c23a24ab3cf9df3812602b7c23e39f2d1a
zjhzhc0218 <136680555@qq.com>
2018-09-17 18:27:28 +0800

    提交项目代码

--- src/main/java/com/deepdraw/deepsearch/controller/LoginControllers.java
+++ src/main/java/com/deepdraw/deepsearch/controller/LoginControllers.java
@@ -5,12 +5,32 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class TestControllers {
+public class LoginControllers {
 
+    /**
+     * 登陆页
+     * @param mv
+     * @return
+     */
     @RequestMapping("/login")
     public ModelAndView toDemo(ModelAndView mv) {
         //mv.addObject("name", "测试1");
         mv.setViewName("index");
         return mv;
     }
+
+
+    /**
+     * 主界面
+     * @param mv
+     * @return
+     */
+    @RequestMapping("/searchIndex")
+    public ModelAndView searchIndex(ModelAndView mv) {
+        //mv.addObject("name", "测试1");
+        mv.setViewName("search");
+        return mv;
+    }
+
+
 }
---
+++ src/main/resources/static/js/loginController.js
@@ -0,0 +1,23 @@
+var app=angular.module('login',[])
+    .controller('loginController',['$scope', function ($scope) {
+        $scope.name = null;
+        $scope.password = null;
+
+        //登陆
+        $scope.login = function () {
+            if ($scope.name != null && $scope.password !=null){
+                console.log('ok');
+            }else {
+                alert('填写信息有误');
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }])
