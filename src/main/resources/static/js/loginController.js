80fed8b688c0c5ad00d5705e0b65bd21313c034e
zyiwei <942560375@qq.com>
2018-09-19 08:40:02 +0800

    人员信息的查询编写

--- src/main/resources/static/js/loginController.js
+++ src/main/resources/static/js/loginController.js
@@ -1,5 +1,5 @@
 var app=angular.module('login',[])
-    .controller('loginController',['$scope', function ($scope) {
+    .controller('loginController', function ($scope,$http) {
         $scope.name = null;
         $scope.password = null;
 
@@ -8,16 +8,20 @@ var app=angular.module('login',[])
             if ($scope.name != null && $scope.password !=null){
                 console.log('ok');
                 $http({
-                    method:'get',
-                    url:'http://127.0.0.1:8080/passwordToLogin'
-                }).then(function successCallback(response) {
-                    alert('成功了');
-                },function errorCallback(response) {
+                    method:'GET',
+                    url:'http://127.0.0.1:8080/passwordToLogin',
+                    params:{
+                        'name':$scope.name,
+                        'password':$scope.password
+                }
+                }).success(function (data) {
+                    alert("成功了"+data);
+                }).error(function (data) {
                     alert("失败了");
-                })
+                });
             }else {
                 alert('填写信息有误');
             }
         }
-    }])
+    })
 
