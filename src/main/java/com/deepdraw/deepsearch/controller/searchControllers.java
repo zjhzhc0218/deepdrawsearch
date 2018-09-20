a832958e8c2f255f53c54fe02cff41afb4946cf7
zjhzhc0218 <136680555@qq.com>
2018-09-20 18:47:00 +0800

    提交项目代码--排名，隐形

--- src/main/java/com/deepdraw/deepsearch/controller/searchControllers.java
+++ src/main/java/com/deepdraw/deepsearch/controller/searchControllers.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 查询控制器
+ */
 @RestController
 public class searchControllers {
-
 
     /**
      * 调用python返回违禁词数据
      * @param request
-     * @param response
      * @return
      */
     @GetMapping(value="/getSearchWjc")
-    public String  findOneUser(HttpServletRequest request,HttpServletResponse response){
+    public String  findOneUser(HttpServletRequest request){
         String word =  request.getParameter("searchWords");
         String[]  args = new String[] { "python", "C:/Users/hc/Desktop/saas/text2.py", word };
         return JsonUtil.object2Json(ResultUtil.success(JavaToPython.getPython(args)));
@@ -29,18 +29,48 @@ public class searchControllers {
     /**
      * 调用python返回信誉数据
      * @param request
-     * @param response
      * @return
      */
     @GetMapping(value="/getSearchXinyu")
-    public String  getSearchXinyu(HttpServletRequest request,HttpServletResponse response){
+    public String  getSearchXinyu(HttpServletRequest request){
         String word =  request.getParameter("searchWords");
         String[]  args = new String[] { "python", "C:/Users/hc/Desktop/saas/text3.py", word};
         String result = JavaToPython.getPython(args);
         if (result.equals("001"))
-            return JsonUtil.object2Json(ResultUtil.error(1,"'该号不存在，请检测是否输入有误'"));
+            return JsonUtil.object2Json(ResultUtil.error(1,"该号不存在，请检测是否输入有误"));
         return JsonUtil.object2Json(ResultUtil.success(result));
     }
 
+    /**
+     * 调用python返回降权数据
+     * @param request
+     * @return
+     */
+    @GetMapping(value="/getSearchJiangQuan")
+    public String  getSearchJiangQuan(HttpServletRequest request){
+        String word =  request.getParameter("searchWords");
+        String isNormal = request.getParameter("isNormal");
+        String[]  args = new String[] { "python", "C:/Users/hc/Desktop/saas/text1.py", word,isNormal};
+        String result = JavaToPython.getPython(args);
+        if (result.equals("002"))
+            return JsonUtil.object2Json(ResultUtil.error(1,"没有获取到此用户的宝贝信息！请稍后重试，谢谢！"));
+        return JsonUtil.object2Json(ResultUtil.success(result));
+    }
+
+    /**
+     * 调用python返回排名数据
+     * @param request
+     * @return
+     */
+    @GetMapping(value="/getSearchPaiming")
+    public String  getSearchPaiming(HttpServletRequest request){
+        String keyWords =  request.getParameter("keyWords");
+        String tbaoId = request.getParameter("tbaoId");
+        String[]  args = new String[] { "python", "C:/Users/hc/Desktop/saas/text4_bRanking.py", keyWords,tbaoId};
+        String result = JavaToPython.getPython(args);
+        if (result!=null && result.equals("003"))
+            return JsonUtil.object2Json(ResultUtil.error(1,"您查询的宝贝不存在"));
+        return JsonUtil.object2Json(ResultUtil.success(result));
+    }
 
 }
--- src/main/resources/static/js/searchController.js
+++ src/main/resources/static/js/searchController.js
@@ -61,7 +61,7 @@ var app=angular.module('search',[])
                 }
             },function errorCallback(info) {
                 $scope.app.working = false;
-                alert("失败了");
+                // alert("失败了");
             })
         }
     //########################违禁词###############################//
@@ -114,11 +114,120 @@ var app=angular.module('search',[])
             }
              $scope.xinYu.working = false;
          },function errorCallback(info) {
-             alert("失败了");
+             // alert("失败了");
              $scope.xinYu.working = false;
          })
      }
      //########################信誉查询###############################//
+
+
+     //########################降权查询###############################//
+
+    $scope.jiangquan = {
+        searchWordsJq : null,//查询
+        working : false,
+        msg : null,
+        isNormal : 0,
+        examedContext : null
+    };
+
+     //查询
+    $scope.searchJiangQuan = function () {
+        if( $scope.jiangquan.searchWordsJq == null) {
+            alert("请输入查询字符");
+            return;
+        }
+        $scope.jiangquan.msg = null;
+        $scope.jiangquan.examedContext = null;
+        $scope.jiangquan.working = true;
+        $http({
+            method:'get',
+            url:'/getSearchJiangQuan', params:{"searchWords": $scope.jiangquan.searchWordsJq,"isNormal":$scope.jiangquan.isNormal},
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            transformRequest: function ( data ) {
+                var str = '';
+                for( var i in data ) {
+                    str += i + '=' + data[i] + '&';
+                }
+            }}
+        ).then(function successCallback(info) {
+            if (info.data.code == 1) {
+                // alert(info.data.msg);
+                $scope.jiangquan.msg = info.data.msg;
+            }else if(info.data.code == 0){
+                var result = info.data.data;
+                $scope.jiangquan.examedContext = angular.fromJson(result.replace(/'/g, '"'));
+            }
+            $scope.jiangquan.working = false;
+        },function errorCallback(info) {
+            // alert("失败了");
+            $scope.jiangquan.working = false;
+        })
+    };
+
+     //########################降权查询###############################//
+
+
+     //########################宝贝排名查询###############################//
+
+    $scope.bbPaiMing = {
+        keyWords : null,//查询
+        tbaoId : null,//查询
+        working : false,
+        msg : null,
+        examedContext : null
+    };
+        //查询
+        $scope.searchPaiming = function () {
+            if ($scope.bbPaiMing.keyWords == null || $scope.bbPaiMing.tbaoId == null) {
+                alert("请输入查询字符");
+                return;
+            }
+            $scope.bbPaiMing.msg = null;
+            $scope.bbPaiMing.examedContext = null;
+            $scope.bbPaiMing.working = true;
+            $scope.ad_oneTimeout=setTimeout(function(){
+                $scope.bbPaiMing.msg = "查询超时！请重新查询";
+                $scope.bbPaiMing.working = false;
+                $scope.$apply();
+            },180000);
+         $http({
+                    method: 'get',
+                    url: '/getSearchPaiming',
+                    params: {"keyWords": $scope.bbPaiMing.keyWords, "tbaoId": $scope.bbPaiMing.tbaoId},
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    transformRequest: function (data) {
+                        var str = '';
+                        for (var i in data) {
+                            str += i + '=' + data[i] + '&';
+                        }
+                    }
+                }
+            ).then(function successCallback(info) {
+                if (info.data.code == 1) {
+                    // alert(info.data.msg);
+                    $scope.bbPaiMing.msg = info.data.msg;
+                } else if (info.data.code == 0) {
+                    var result = info.data.data;
+                    $scope.bbPaiMing.examedContext = angular.fromJson(result.replace(/'/g, '"'));
+                }
+                $scope.bbPaiMing.working = false;
+                clearTimeout($scope.ad_oneTimeout);
+            }, function errorCallback(info) {
+                // alert("失败了");
+                $scope.bbPaiMing.working = false;
+                clearTimeout($scope.ad_oneTimeout);
+            })
+        }
+
+
+
+
+
+
+
+
+     //########################宝贝排名查询###############################//
 
 
 
