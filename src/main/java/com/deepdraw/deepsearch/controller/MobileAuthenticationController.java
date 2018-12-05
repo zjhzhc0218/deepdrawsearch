70c370706f79308269836c945134626d98ae701c
zyiwei <942560375@qq.com>
2018-12-05 20:34:55 +0800

    更换白马查

--- src/main/java/com/deepdraw/deepsearch/controller/MobileAuthenticationController.java
+++ src/main/java/com/deepdraw/deepsearch/controller/MobileAuthenticationController.java
@@ -44,8 +44,8 @@ public class MobileAuthenticationController {
     final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
     final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
     // 替换成你的AK
-    final String accessKeyId = "LTAIsYjzA8zRbFLW";// 你的accessKeyId
-    final String accessKeySecret = "tyky68zg3xoWdEOwYvD6sTxwDMXDfW";// 你的accessKeySecret
+    final String accessKeyId = "LTAIjKquZdOADXca";// 你的accessKeyId
+    final String accessKeySecret = "alGA4Y9kUDLHJg6gPm0mFk4e9pTRhm";// 你的accessKeySecret
     // 初始化ascClient,暂时不支持多region
     IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product,
@@ -58,9 +58,9 @@ public class MobileAuthenticationController {
     // 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时   性稍有延迟,验证码类型的短信推荐使用单条调用的方式
         request.setPhoneNumbers(PhoneNumbers);
     // 必填:短信签名-可在短信控制台中找到
-        request.setSignName("zero");
+        request.setSignName("白马查");
     // 必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_137675098");
+        request.setTemplateCode("SMS_152287053");
     //request.setTemplateCode("SMS_116580423");
     // 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
     // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
