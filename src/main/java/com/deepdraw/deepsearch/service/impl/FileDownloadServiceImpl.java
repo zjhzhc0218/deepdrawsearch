c74bf06ceaf7ed5b2215d3470a463292d92c279d
zyiwei <942560375@qq.com>
2019-01-19 17:15:00 +0800

    修改对应的分页数据

--- src/main/java/com/deepdraw/deepsearch/service/impl/FileDownloadServiceImpl.java
+++ src/main/java/com/deepdraw/deepsearch/service/impl/FileDownloadServiceImpl.java
@@ -78,7 +78,7 @@ public class FileDownloadServiceImpl implements FileDownloadService{
 
     @Override
     public List<FileDownload> selectPage(Integer p, Integer num) {
-        Integer number1 = num*(p-1)+1;
+        Integer number1 = num*(p-1);
         Integer number2 = num*p;
         return fileDownloadDao.selectFDbyLimit(number1,number2);
     }
